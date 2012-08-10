65c591c5ef97f4f51bc7331bac62a678afbf8380
Pau Cervera <pau.cervera@gmail.com>
2012-08-10 18:25:10 +0200

    csv encoded per 1 registre

--- csv.js
+++ csv.js
@@ -1,14 +1,39 @@
-function to_csv (headers, array) {
+
+var encode = function (obj) { return encodeURIComponent(obj); };
+
+var to_csv = function (headers, array) {
     var csv = '';
-    return csv;
+    for (var i in headers) {
+	csv += headers[i];
+	csv += ';';
+    }
+    csv = csv.substring(0, csv.length - 1);
+    csv += '\n';
+    for (var i = 0, len = array.length; i < len; i++) {
+	for (var j in headers) {	    
+	    csv += array[i][headers[j]] || '';
+	    csv += ';';
+	}
+	csv = csv.substring(0, csv.length - 1);
+	csv += '\n';
+    }
+    csv = csv.substring(0,csv.length - 1);
+    return encode(csv);
 };
 
+// nodeunit tests
 var assert = require('assert');
 exports = exports || {};
 exports['csv'] = function (test) {
-    var headers = undefined;
-    var array = undefined;
+    var headers = ['Name', 'Surname', 'Age'];
+    var array = [];
+    array.push({
+	Name: 'Pau',
+	Surname: 'Cervera',
+	Age: 34
+    });
     var csv = to_csv(headers, array);
-    test.deepEqual(csv, '');
+    console.log(csv);
+    test.deepEqual(csv, encode('Name;Surname;Age\nPau;Cervera;34'));
     test.done();
 };
